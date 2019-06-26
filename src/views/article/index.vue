9c6d6c4c3f42a817fbff9f479d3cbb62af967cb1
Wsy-Xm <15097317238@163.com>
2019-06-26 21:10:07 +0800

    内容管理路由配置成功

---
+++ src/views/article/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <p>article</p>
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style lang="less">
+
+</style>
--- src/views/layout/components/AppAside.vue
+++ src/views/layout/components/AppAside.vue
@@ -21,7 +21,7 @@
         </template>
         <el-menu-item-group>
           <el-menu-item index="/publish">发布文章</el-menu-item>
-          <el-menu-item index="2-2">内容管理</el-menu-item>
+          <el-menu-item index="/article">内容管理</el-menu-item>
           <el-menu-item index="2-3">评论列表</el-menu-item>
           <el-menu-item index="2-4">素材管理</el-menu-item>
         </el-menu-item-group>
